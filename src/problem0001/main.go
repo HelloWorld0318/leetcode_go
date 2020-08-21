7c4c24a950abea1dec84fb7ffe8eafdfab8fdad5
houjinxiang <houjinxiang@bilibili.com>
2020-08-21 15:18:26 +0800

    添加解析shard的代码

--- src/problem0001/main.go
+++ src/problem0001/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+const _timeStamp20170701 = 1498838400
 
 func main() {
 	target := 9
 	nums := []int{2, 7, 11, 15}
 	result := twoSum(nums, target)
 	fmt.Println(result)
+
+	fmt.Println("============")
+
+	shardInfo, _ := shardDynId(425867834429550682)
+	fmt.Println("year:",shardInfo.Year)
+	fmt.Println("month:",shardInfo.Month)
+	fmt.Println("rand:",shardInfo.Rand)
+
+	fmt.Println("============")
+	blockUsers := make(map[int64]ExpiredTs)
+	blockUsers[0] = ExpiredTs{ExpiredTs: 0}
+	blockUsersBys, _ := json.Marshal(blockUsers)
+	fmt.Println(string(blockUsersBys))
+
+	fmt.Println(len("bk:1:212295231"))
 }
 
 func twoSum(nums []int, target int) []int {
@@ -20,4 +41,25 @@ func twoSum(nums []int, target int) []int {
 		hash[v] = index
 	}
 	return []int{}
+}
+
+func shardDynId(dynamicID uint64) (shardInfo *ShardInfo, err error) {
+	ts := (dynamicID >> 32) + _timeStamp20170701
+	tm := time.Unix(int64(ts), 0)
+	shardInfo = &ShardInfo{
+		Year:  int32(tm.Year()),
+		Month: int32(tm.Month()),
+		Rand:  (dynamicID & 0xffffffff) >> 20,
+	}
+	return
+}
+
+type ShardInfo struct {
+	Year  int32
+	Month int32
+	Rand  uint64
+}
+
+type ExpiredTs struct {
+	ExpiredTs int64 `json:"ex_ts"`
 }
