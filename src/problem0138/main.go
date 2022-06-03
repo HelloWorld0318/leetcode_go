5205a584a3a6f0357fd30785bbc7244adc7f932c
houjinxiang <houjinxiang@bilibili.com>
2022-06-04 01:12:08 +0800

    add

--- src/problem0138/main.go
+++ src/problem0138/main.go
@@ -6,7 +6,7 @@ type Node struct {
 	Random *Node
 }
 
-func copyRandomList(head *Node) *Node {
+func copyRandomList1(head *Node) *Node {
 	if head == nil {
 		return nil
 	}
@@ -27,7 +27,6 @@ func copyRandomList(head *Node) *Node {
 	}
 
 	dummy := &Node{Val: -1}
-
 	curNode = head
 	node := dummy
 	for curNode != nil {
@@ -38,4 +37,34 @@ func copyRandomList(head *Node) *Node {
 		curNode = curNode.Next
 	}
 	return dummy.Next
+}
+
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	nodeMap := make(map[*Node]int)
+	nodeList := make([]*Node, 0)
+	cur, index := head, 0
+	for cur != nil {
+		nodeMap[cur] = index
+		nodeList = append(nodeList, &Node{Val: cur.Val})
+		index++
+		cur = cur.Next
+	}
+
+	cur = head
+	i := 0
+	for cur != nil {
+		if i+1 < len(nodeList) {
+			nodeList[i].Next = nodeList[i+1]
+		}
+		if cur.Random != nil {
+			index := nodeMap[cur.Random]
+			nodeList[i].Random = nodeList[index]
+		}
+		i++
+		cur = cur.Next
+	}
+	return nodeList[0]
 }
