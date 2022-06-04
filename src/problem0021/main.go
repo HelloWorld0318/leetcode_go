a6896d6088b6cfa620b55ecccf75e7a622c86f52
houjinxiang <houjinxiang@bilibili.com>
2022-06-04 08:49:51 +0800

    add

--- src/problem0021/main.go
+++ src/problem0021/main.go
@@ -8,8 +8,8 @@ type ListNode struct {
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	dummy := &ListNode{Val: -1}
 	cur := dummy
-	for list1 != nil && list2 != nil {
-		if list1.Val <= list2.Val {
+	for list1 != nil || list2 != nil {
+		if list2 == nil || (list1 != nil && list1.Val <= list2.Val) {
 			cur.Next = list1
 			list1 = list1.Next
 		} else {
@@ -17,12 +17,6 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 			list2 = list2.Next
 		}
 		cur = cur.Next
-	}
-	if list1 != nil {
-		cur.Next = list1
-	}
-	if list2 != nil {
-		cur.Next = list2
 	}
 	return dummy.Next
 }
