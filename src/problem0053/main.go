14ef5e376b839753cacf29d3e07f6c57c4011edb
houjinxiang <houjinxiang@bilibili.com>
2022-08-11 17:29:59 +0800

    add

--- src/problem0053/main.go
+++ src/problem0053/main.go
@@ -1,13 +1,17 @@
 package problem0053
 
 func maxSubArray(nums []int) int {
+	//将求n个数的数组的最大子段和，转换成分别求以第1个，第2个，、、、、，第i个，、、、
+	//第n个数字结尾的最大子段和，再找出这n个结果中最大的，即为结果
+	//若dp[i-1]>0，dp[i] = dp[i-1] + nums[i]；否则：dp[i] = nums[i]
+	//边界值：以第1个数字结尾的最大子段和dp[0]=nums[0]
 	dp, i := make([]int, len(nums)), 0
 	for i < len(nums) {
 		if i == 0 {
 			dp[0] = nums[0]
 		} else {
 			dp[i] = nums[i]
-			if nums[i]+dp[i-1] > nums[i] {
+			if dp[i-1] > 0 {
 				dp[i] = nums[i] + dp[i-1]
 			}
 		}
