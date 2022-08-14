3f6e45998b8f2b6b183a8be9e7f5407aafb91e5e
houjinxiang <houjinxiang@bilibili.com>
2022-08-15 00:53:44 +0800

    add

--- src/problem0200/main.go
+++ src/problem0200/main.go
@@ -22,7 +22,7 @@ func numIslands(grid [][]byte) int {
 		j := 0
 		for j < column {
 			if grid[i][j] == '1' && mark[i][j] == 0 {
-				dfs(&grid, &mark, i, j)
+				bfs(grid, mark, i, j)
 				numOfLands++
 			}
 			j++
@@ -32,18 +32,44 @@ func numIslands(grid [][]byte) int {
 	return numOfLands
 }
 
-func dfs(grid *[][]byte, mark *[][]int, x int, y int) {
-	(*mark)[x][y] = 1
+func dfs(grid [][]byte, mark [][]int, x int, y int) {
+	mark[x][y] = 1
 	//朝方向数组4个方向遍历深度优先搜索
-	row, column := len(*grid), len((*grid)[0])
+	row, column := len(grid), len(grid[0])
 	i := 0
 	for i < 4 {
 		newX := x + dx[i]
 		newY := y + dy[i]
-		if newX >= 0 && newX < row && newY >= 0 && newY < column && (*grid)[newX][newY] == '1' &&
-			(*mark)[newX][newY] == 0 {
+		if newX >= 0 && newX < row && newY >= 0 && newY < column && grid[newX][newY] == '1' &&
+			mark[newX][newY] == 0 {
 			dfs(grid, mark, newX, newY)
 		}
 		i++
 	}
+}
+
+func bfs(grid [][]byte, mark [][]int, x int, y int) {
+	mark[x][y] = 1
+	queue := make([]*Pair, 0)
+	pair := &Pair{X: x, Y: y}
+	queue = append(queue, pair)
+	for len(queue) != 0 {
+		pair, queue = queue[0], queue[1:]
+		i := 0
+		for i < 4 {
+			newX := pair.X + dx[i]
+			newY := pair.Y + dy[i]
+			for newX >= 0 && newX < len(grid) && newY >= 0 && newY < len(grid[0]) && grid[newX][newY] == '1' &&
+				mark[newX][newY] == 0 {
+				mark[newX][newY] = 1
+				queue = append(queue, &Pair{X: newX, Y: newY})
+			}
+			i++
+		}
+	}
+}
+
+type Pair struct {
+	X int
+	Y int
 }
